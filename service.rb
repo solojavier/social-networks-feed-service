8985040bf14f350fdaefa4ad94348611778161f0
solojavier <1.27201@gmail.com>
2012-11-06 15:24:01 -0600

    Adding a little logic to controller

--- service.rb
+++ service.rb
@@ -9,12 +9,24 @@ get '/resources' do
   Resource.all.to_json
 end
 
-  Test.find(params[:id].to_i).to_json
 get '/resources/:id' do
+  begin
     Resource.find(params[:id].to_i).to_json
+  rescue
+    404
+  end
 end
 
 post '/resources' do
+  return 422 if params.has_key?('invalid_attribute')
+
+  resource = Resource.new(params)
+
+  if resource.save
+    resource.to_json
+  else
+    500
+  end
 end
 
 get '/' do
