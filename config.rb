452667e7ccdb5940fdf68bc04c1f79fc355616a4
solojavier <1.27201@gmail.com>
2012-11-08 14:58:32 -0600

    Moving global variables to config file

--- config.rb
+++ config.rb
@@ -3,3 +3,13 @@ TWITTER_CONSUMER_SECRET    = ''
 TWITTER_OAUTH_TOKEN        = ''
 TWITTER_OAUTH_TOKEN_SECRET = ''
 FACEBOOK_TOKEN             = ''
+
+FACEBOOK_HOME_URL = "https://graph.facebook.com/me/home?access_token=#{FACEBOOK_TOKEN}"
+FACEBOOK_POST_URL = "https://graph.facebook.com/me/feed?access_token=#{FACEBOOK_TOKEN}"
+
+Twitter.configure do |config|
+  config.consumer_key       = TWITTER_CONSUMER_KEY
+  config.consumer_secret    = TWITTER_CONSUMER_SECRET
+  config.oauth_token        = TWITTER_OAUTH_TOKEN
+  config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
+end
